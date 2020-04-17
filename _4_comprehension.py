b22d7059603ca1b2f285cf82c2dd5f356309c9d7
wlgud0402 <wlgudrlgus@naver.com>
2020-04-17 15:30:36 +0900

    2020-04-17

---
+++ _4_comprehension.py
@@ -0,0 +1,8 @@
+even = [number for number in range(10) if number % 2 == 0 ]
+print(even)
+
+print()
+print()
+
+squares = {key:key*key for key in range(10)}
+print(squares)
---
+++ _4_generator.py
@@ -0,0 +1,8 @@
+def get_odds():
+    for number in range(1,10,2):
+        yield number
+
+for count, number in enumerate(get_odds(),1):
+    if count == 3:
+        print("the third odd number is :",number)
+
