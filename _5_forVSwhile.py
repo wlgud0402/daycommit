8c573bd0445466493bd2bdac5dec8a9837212012
wlgud0402 <wlgudrlgus@naver.com>
2020-04-18 21:54:34 +0900

    2020-04-18

---
+++ _5_forVSwhile.py
@@ -0,0 +1,16 @@
+#for 문과 while문의 차이
+#둘 모두 반복을 돌리는 것이다 하지만 while은 ~인동안 계속하는것이고 for문은 조건을 지정해준다.
+
+for i in range(1,11):
+    print(i)
+    i +=1
+
+print()
+print()
+print()
+
+
+i=1
+while i<11:
+    print(i)
+    i +=1
